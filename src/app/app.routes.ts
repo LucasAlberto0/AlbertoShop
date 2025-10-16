8521c0e8f5efa2c7bb6bf674a98679e88c9124ab
Lucas Alberto <lucasalberto4321@gmail.com>
2025-10-16 16:53:23 -0300

    setup-inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Home } from './pages/home/home';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    }
+];
