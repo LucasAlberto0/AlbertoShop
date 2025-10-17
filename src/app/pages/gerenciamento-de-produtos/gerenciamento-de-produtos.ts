fd5e603d4b9153bc56d841c5b7b83b57cdd74e79
Lucas Alberto <lucasalberto4321@gmail.com>
2025-10-17 15:30:40 -0300

    feat: box-da-lista-de-produtos

--- src/app/pages/gerenciamento-de-produtos/gerenciamento-de-produtos.ts
+++ src/app/pages/gerenciamento-de-produtos/gerenciamento-de-produtos.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderGerenciadorDeProdutos } from "../../components/header-gerenciador-de-produtos/header-gerenciador-de-produtos";
 import { TituloDaListaDeProdutos } from "../../components/titulo-da-lista-de-produtos/titulo-da-lista-de-produtos";
+import { ListaDeProdutos } from "../../components/lista-de-produtos/lista-de-produtos";
 
 @Component({
   selector: 'app-gerenciamento-de-produtos',
-  imports: [HeaderGerenciadorDeProdutos, TituloDaListaDeProdutos],
+  imports: [HeaderGerenciadorDeProdutos, TituloDaListaDeProdutos, ListaDeProdutos],
   templateUrl: './gerenciamento-de-produtos.html',
   styleUrl: './gerenciamento-de-produtos.scss'
 })
