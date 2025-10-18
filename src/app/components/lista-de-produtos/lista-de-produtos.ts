c71eb0b6af58b2908a1ccc48525eb7a9dbc011b5
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-10-18 18:53:04 -0300

    feat: verbos-http-service

--- src/app/components/lista-de-produtos/lista-de-produtos.ts
+++ src/app/components/lista-de-produtos/lista-de-produtos.ts
@@ -1,18 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-
-export interface ProdutosInterface {
-  nome: string;
-  descricao: string;
-  preco: number;
-  estoque: number;
-}
-
-const ELEMENT_DATA: ProdutosInterface[] = [
-  { nome: 'Notebook Dell', descricao: 'Core i5, 8GB RAM', preco: 3.500, estoque: 15 },
-  { nome: 'Mouse Logitech', descricao: 'Wireless, ergonômico', preco: 150, estoque: 50 },
-  { nome: 'Teclado Mecânico', descricao: 'RGB, switches blue', preco: 450, estoque: 30 },
-];
+import { ProdutosService } from '../../services/produtos-service';
+import { IProduto } from '../../interfaces/IprodutoInterface';
 
 @Component({
   selector: 'app-lista-de-produtos',
@@ -20,7 +9,24 @@ const ELEMENT_DATA: ProdutosInterface[] = [
   templateUrl: './lista-de-produtos.html',
   styleUrl: './lista-de-produtos.scss'
 })
-export class ListaDeProdutos {
+export class ListaDeProdutos implements OnInit {
   displayedColumns: string[] = ['nome', 'descricao', 'preco', 'estoque', 'acoes' ];
-  dataSource = ELEMENT_DATA;
+  dataSource: IProduto[] = [];
+
+  constructor(private _produtoService: ProdutosService) {}
+
+  ngOnInit(): void {
+    this.carregarProdutos();
+  }
+
+  carregarProdutos() {
+    this._produtoService.listarProdutos().subscribe({
+      next: (data) => (this.dataSource = data),
+      error: (err) => console.error('Erro ao buscar produtos', err)
+    });
+  }
+
+  deletarProduto(id: number) {
+    this._produtoService.deletarProduto(id).subscribe(() => this.carregarProdutos());
+  }
 }
---
+++ src/app/services/produtos-service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduto } from '../interfaces/IprodutoInterface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+  private apiUrl = 'http://localhost:3000/produtos'
+  
+
+  constructor(private _http: HttpClient ) {}
+
+  listarProdutos(): Observable<IProduto[]> {
+    return this._http.get<IProduto []>(this.apiUrl);
+  }
+
+  criarProduto(produto: IProduto): Observable<IProduto> {
+    return this._http.post<IProduto>(this.apiUrl, produto);
+  }
+
+  deletarProduto(id: number): Observable<any> {
+    return this._http.delete(`${this.apiUrl}/${id}`);
+  }
+}
